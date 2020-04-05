45b45fd34b87ee786ac44887d887407833f69231
joaoDossena <joaopdossena@gmail.com>
2020-04-05 16:50:56 +0100

    Updating criar.sql and povoar.sql

--- 2nd Submission/criar.sql
+++ 2nd Submission/criar.sql
@@ -1,19 +1,139 @@
 PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
-CREATE TABLE PessoaFEUP (id STRING PRIMARY KEY UNIQUE, nome STRING, morada STRING, dataNasc DATE, sexo CHAR CHECK (sexo LIKE 'M' OR 'F'), NIF INTEGER UNIQUE, telefone INTEGER UNIQUE);
-CREATE TABLE Estudante (id REFERENCES PessoaFEUP (id) PRIMARY KEY UNIQUE, anoInscricao INTEGER, idCurso REFERENCES Curso (idCurso));
-CREATE TABLE Docente (id STRING REFERENCES PessoaFEUP (id) PRIMARY KEY UNIQUE, categoria STRING, departamento STRING);
-CREATE TABLE Curso (idCurso STRING PRIMARY KEY UNIQUE, nome STRING, grauAcademico STRING, duracao INTEGER, mediaEntrada INTEGER CHECK (mediaEntrada >= 0 AND mediaEntrada <= 20), dataInicio DATE);
-CREATE TABLE Cadeira (cadeiraID STRING PRIMARY KEY UNIQUE, nome STRING, creditos INTEGER, idCurso STRING REFERENCES Curso (idCurso));
-CREATE TABLE OcorrenciaCadeira (idOcorrenciaCadeira STRING PRIMARY KEY UNIQUE, cadeiraID STRING REFERENCES Cadeira (codigo));
-CREATE TABLE Epoca (idEpoca STRING PRIMARY KEY UNIQUE, anoLetivo STRING, semestre STRING CHECK (semestre LIKE "Primeiro" OR "Segundo"));
-CREATE TABLE ComponenteAvaliacao (idComponenteAvaliacao STRING PRIMARY KEY UNIQUE, nome STRING, idOcorrenciaCadeira STRING REFERENCES OcorrenciaCadeira (idOcorrenciaCadeira));
-CREATE TABLE Nota (idNota STRING PRIMARY KEY UNIQUE, nota DOUBLE, idComponenteAvaliacao STRING REFERENCES ComponenteAvaliacao (idComponenteAvaliacao));
-CREATE TABLE EstudanteOcorrenciaCadeira (id STRING REFERENCES Estudante (id), idOcorrenciaCadeira STRING REFERENCES OcorrenciaCadeira (idOcorrenciaCadeira));
-CREATE TABLE EstudanteComponenteAvaliacao (id STRING REFERENCES Estudante (id), idComponenteAvaliacao STRING REFERENCES ComponenteAvaliacao (idComponenteAvaliacao));
-CREATE TABLE EstudanteTurmaPratica (id STRING REFERENCES Estudante (id), idTurmaPratica STRING REFERENCES TurmaPratica (idTurmaPratica));
-CREATE TABLE DocenteTurmaPratica (id STRING REFERENCES Docente (id), idTurmaPratica STRING REFERENCES TurmaPratica (idTurmaPratica));
-CREATE TABLE CadeiraDocente (id STRING REFERENCES Docente (id), cadeiraID STRING REFERENCES Cadeira (cadeiraID));
-CREATE TABLE OcorrenciaCadeiraEpoca (idOcorrenciaCadeira STRING REFERENCES OcorrenciaCadeira (idOcorrenciaCadeira), idEpoca STRING REFERENCES Epoca (idEpoca));
-CREATE TABLE TurmaPratica (idTurmaPratica STRING PRIMARY KEY UNIQUE, numEstudantes INTEGER, diaSemana STRING CHECK (diaSemana LIKE "Segunda-Feira" OR "Terça-Feira" OR "Quarta-Feira" OR "Quinta-Feira" OR "Sexta-Feira"), horaInicio TIME, cadeiraID STRING REFERENCES Cadeira (cadeiraID), horaFim TIME);
-COMMIT;
+
+--Estudante
+DROP TABLE IF EXISTS Estudante;
+CREATE TABLE Estudante (
+	id REFERENCES PessoaFEUP (id) PRIMARY KEY UNIQUE,
+	anoInscricao INTEGER,
+	idCurso REFERENCES Curso (idCurso)
+);
+
+--Docente
+DROP TABLE IF EXISTS Docente;
+CREATE TABLE Docente (
+	id STRING REFERENCES PessoaFEUP (id) PRIMARY KEY UNIQUE,
+	categoria STRING,
+	departamento STRING
+);
+
+--Curso
+DROP TABLE IF EXISTS Curso;
+CREATE TABLE Curso (
+	idCurso STRING PRIMARY KEY UNIQUE,
+	nome STRING,
+	grauAcademico STRING,
+	duracao INTEGER,
+	mediaEntrada INTEGER CHECK (mediaEntrada >= 0 AND mediaEntrada <= 20),
+	dataInicio DATE
+);
+
+--Cadeira
+DROP TABLE IF EXISTS Cadeira;
+CREATE TABLE Cadeira (
+	cadeiraID STRING PRIMARY KEY UNIQUE,
+	nome STRING,
+	creditos INTEGER,
+	idCurso STRING REFERENCES Curso (idCurso)
+);
+
+--OcorrenciaCadeira
+DROP TABLE IF EXISTS OcorrenciaCadeira;
+CREATE TABLE OcorrenciaCadeira (
+	idOcorrenciaCadeira STRING PRIMARY KEY UNIQUE,
+	cadeiraID STRING REFERENCES Cadeira (codigo)
+);
+
+--Epoca
+DROP TABLE IF EXISTS Epoca;
+CREATE TABLE Epoca (
+	idEpoca STRING PRIMARY KEY UNIQUE,
+	anoLetivo STRING,
+	semestre STRING CHECK (semestre LIKE "Primeiro" OR "Segundo")
+);
+
+--ComponenteAvaliacao
+DROP TABLE IF EXISTS ComponenteAvaliacao;
+CREATE TABLE ComponenteAvaliacao (
+	idComponenteAvaliacao STRING PRIMARY KEY UNIQUE,
+	nome STRING,
+	idOcorrenciaCadeira STRING REFERENCES OcorrenciaCadeira (idOcorrenciaCadeira)
+);
+
+--Nota
+DROP TABLE IF EXISTS Nota;
+CREATE TABLE Nota (
+	idNota STRING PRIMARY KEY UNIQUE,
+	nota DOUBLE,
+	idComponenteAvaliacao STRING REFERENCES ComponenteAvaliacao (idComponenteAvaliacao)
+);
+
+--EstudanteOcorrenciaCadeira
+DROP TABLE IF EXISTS EstudanteOcorrenciaCadeira;
+CREATE TABLE EstudanteOcorrenciaCadeira (
+	id STRING REFERENCES Estudante (id),
+	idOcorrenciaCadeira STRING REFERENCES OcorrenciaCadeira (idOcorrenciaCadeira)
+);
+
+--EstudanteComponenteAvaliacao
+DROP TABLE IF EXISTS EstudanteComponenteAvaliacao;
+CREATE TABLE EstudanteComponenteAvaliacao (
+	id STRING REFERENCES Estudante (id),
+	idComponenteAvaliacao STRING REFERENCES ComponenteAvaliacao (idComponenteAvaliacao)
+);
+
+--EstudanteTurmaPratica
+DROP TABLE IF EXISTS EstudanteTurmaPratica;
+CREATE TABLE EstudanteTurmaPratica (
+	id STRING REFERENCES Estudante (id),
+	idTurmaPratica STRING REFERENCES TurmaPratica (idTurmaPratica)
+);
+
+--DocenteTurmaPratica
+DROP TABLE IF EXISTS DocenteTurmaPratica;
+CREATE TABLE DocenteTurmaPratica (
+	id STRING REFERENCES Docente (id),
+	idTurmaPratica STRING REFERENCES TurmaPratica (idTurmaPratica)
+);
+
+--CadeiraDocente
+DROP TABLE IF EXISTS CadeiraDocente;
+CREATE TABLE CadeiraDocente (
+	id STRING REFERENCES Docente (id),
+	cadeiraID STRING REFERENCES Cadeira (cadeiraID)
+);
+
+--OcorrenciaCadeiraEpoca
+DROP TABLE IF EXISTS OcorrenciaCadeiraEpoca;
+CREATE TABLE OcorrenciaCadeiraEpoca (
+	idOcorrenciaCadeira STRING REFERENCES OcorrenciaCadeira (idOcorrenciaCadeira),
+	idEpoca STRING REFERENCES Epoca (idEpoca)
+);
+
+--TurmaPratica
+DROP TABLE IF EXISTS TurmaPratica;
+CREATE TABLE TurmaPratica (
+	idTurmaPratica STRING PRIMARY KEY UNIQUE,
+	numEstudantes INTEGER,
+	diaSemana STRING CHECK (diaSemana LIKE "Segunda-Feira" OR "Terça-Feira" OR "Quarta-Feira" OR "Quinta-Feira" OR "Sexta-Feira"),
+	horaInicio TIME,
+	horaFim TIME,
+	cadeiraID STRING REFERENCES Cadeira (cadeiraID)
+);
+
+--PessoaFEUP
+DROP TABLE IF EXISTS PessoaFEUP;
+CREATE TABLE PessoaFEUP (
+	id STRING PRIMARY KEY,
+	nome STRING,
+	morada STRING,
+	dataNasc DATE,
+	sexo CHAR CHECK (sexo LIKE 'M' OR 'F'),
+	NIF INTEGER,
+	telefone INTEGER,
+	email STRING
+);
+
+
+COMMIT TRANSACTION;
+PRAGMA foreign_keys = on;
---
+++ 2nd Submission/povoar.sql
@@ -0,0 +1,29 @@
+PRAGMA foreign_keys = on;
+
+--PessoaFEUP info insertion
+
+INSERT INTO PessoaFEUP (id, nome, morada, dataNasc, sexo, NIF, telefone, email)
+VALUES ('UP201800001', 'João Sousa da Silva', 'Rua Lael Ferreira, 429', '1999-01-01', 'M', 393201029, 9384928392, 'jss@gmail.com'),
+    ('UP201800002', 'Joao Melo Araujo', 'Rua Morrinhos, 1018', '2000-01-19', 'M', 902867607, 997375190, 'jma@gmail.com'),
+    ('UP201800003', 'Rebeca Cardoso Correia', 'Rua Antônio Marinho Mendonça, 1397', '2000-10-25', 'F', 463207577, 968918877, 'rcc@gmail.com'),
+    ('UP201800004', 'Carla Correia Almeida', 'Rua Antônio Bernardino da Costa, 547', '1999-05-21', 'F', 358955477, 94521-5071, 'ccalmeida@gmail.com'),
+    ('UP201800005', 'José Silva Gomes', 'Rua Piracicaba, 1419', '1999-06-28', 'M', 886171453, 938543650, 'jsgomes@gmail.com'),
+    ('UP201800006', 'Vitor Goncalves Ribeiro', 'Rua Serafim de Oliveira, 1860', '1999-12-28', 'M', 862776142, 826496716, 'vgribeiro@gmail.com'),
+    ('UP201800007', 'Guilherme Souza Oliveira', 'Rua Francisco Custódio Pereira, 1445', '1999-01-05', 'M', 262306718, 842483925, 'gso@gmail.com'),
+    ('UP201800008', 'Giovanna Almeida Barbosa', 'Rua E, 1101', '2000-10-31', 'F', 832543573, 930505156, 'gabarbosa@gmail.com'),
+    ('UP201800009', 'Joao Sousa Rocha', 'Rua Gotthard Kunzli, 715', '2000-03-17', 'M', 731640385, 847025574, 'jsrocha@gmail.com'),
+    ('UP201800010', 'Marcos Dias Azevedo', 'Rua Zambugueiro, 1542', '1999-07-29', 'M', 254936802, 992326162, 'mda@hotmail.com'),
+    ('UP201800011', 'David Santos Araujo', 'Rua Primeiro de Maio, 1059', '1999-08-29', 'M', 901892778, 824388412, 'dsa@hotmail.com'),
+    ('UP201800012', 'Gustavo Alves Azevedo', 'Rua Giordano Mochel, 393', '2000-01-03', 'M', 120138981, 966733850, 'gaa@yahoo.com'),
+    ('UP201800013', 'Daniel Souza Barros', 'Rua Ilhéus, 1120', '2000-10-05', 'M', 357026295, 954815704, 'dsb@yahoo.com'),
+    ('UP201800014', 'Amanda Rocha Melo', 'Praça Santo Antônio, 999', '2000-11-16', 'F', 966149869, 890908238, 'armelo@hotmail.com'),
+    ('UP201800015', 'Marina Araujo Cunha', 'Rua Pedro Bonafe, 871', '2000-01-07', 'F', 507185029, 982416048, 'mac@gmail.com'),
+    ('UP201800015', 'Isabelle Souza Barbosa', 'Rua Padre Miguel Assis, 736', '2000-02-25', 'F', 519698400, 965528897, 'IsabelleSouzaBarbosa@jourrapide.com'),
+    ('UP201800018', 'Clara Ribeiro Melo', 'Praça Joaquim Pedro Rosa, 1205', '2000-11-26', 'F', 239317462, 69807295, 'ClaraRibeiroMelo@teleworm.us '),
+    ('UP201800019', 'Filipe Sousa Gomes', '1ª Travessa Alto Vila Rica, 1946', '2000-11-04', 'M', 614940534, 950546747, 'FelipeSouzaGomes@armyspy.com '),
+    ('UP201800020', 'Rodrigo Gomes Sousa', 'Beco Novo, 1620', '2000-09-08', 'M', 556386321, 934039307, 'RodrigoGomesSousa@dayrep.com')
+;
+
+
+SELECT *
+FROM PessoaFEUP;
