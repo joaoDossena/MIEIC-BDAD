4052d8515f409a55d2ff0528793d41b35c61adbd
joaoDossena <joaopdossena@gmail.com>
2020-05-23 14:56:12 +0100

    Adding second query

--- 3rd Submission/int2.sql
+++ 3rd Submission/int2.sql
@@ -1,7 +1,11 @@
---descriçao da interrogaçao
+--Lista os tuplos nota, idCadeira, tal que a nota seja menor que 10
 .mode columns
 .headers on
 .nullvalue NULL
 
-SELECT 
-FROM
+SELECT DISTINCT nota, idCadeira
+FROM (Nota JOIN OcorrenciaCadeira JOIN ComponenteAvaliacao)
+WHERE ( (Nota.idComponenteAvaliacao = ComponenteAvaliacao.idComponenteAvaliacao)
+AND (ComponenteAvaliacao.idOcorrenciaCadeira = OcorrenciaCadeira.idOcorrenciaCadeira)
+AND (Nota.nota < 10) )
+ORDER BY nota ASC;
