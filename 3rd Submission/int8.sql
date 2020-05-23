ece1707c3d3b966a8a4073a313d23944dda28aa2
joaoDossena <joaopdossena@gmail.com>
2020-05-23 16:55:59 +0100

    Adding eighth query

--- 3rd Submission/int8.sql
+++ 3rd Submission/int8.sql
@@ -1,5 +1,13 @@
---descriçao da interrogaçao
+--Mostra os Estudantes e a soma do número de créditos das Cadeiras em que estão inscritos
 .mode columns
 .headers on
 .nullvalue NULL
 
+SELECT PessoaFEUP.nome, SUM(Cadeira.creditos)
+FROM (PessoaFEUP JOIN Estudante JOIN EstudanteOcorrenciaCadeira
+	JOIN OcorrenciaCadeira JOIN Cadeira)
+WHERE PessoaFEUP.id = Estudante.id AND Estudante.id = EstudanteOcorrenciaCadeira.id
+	AND EstudanteOcorrenciaCadeira.idOcorrenciaCadeira = OcorrenciaCadeira.idOcorrenciaCadeira
+	AND OcorrenciaCadeira.idCadeira = Cadeira.idCadeira
+GROUP BY PessoaFEUP.nome
+;
