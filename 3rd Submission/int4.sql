614296cd58f172c82230d4fa85bf8b87086f96ff
joaoDossena <joaopdossena@gmail.com>
2020-05-23 15:23:18 +0100

    Adding fourth query

--- 3rd Submission/int4.sql
+++ 3rd Submission/int4.sql
@@ -1,5 +1,10 @@
---descriçao da interrogaçao
+--Lista os horários de início e fim das turmas práticas 01 do curso MIEIC, ordenados por duração da aula
 .mode columns
 .headers on
 .nullvalue NULL
 
+SELECT horaInicio, horaFim, idTurmaPratica
+FROM TurmaPratica
+WHERE idTurmaPratica LIKE 'MIEIC-%01' 
+GROUP BY (horaFim - horaInicio)
+;
