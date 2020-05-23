2b04e1046af5546e3d6b5537e76310046d88f274
joaoDossena <joaopdossena@gmail.com>
2020-05-23 18:37:24 +0100

    Adding tenth query

--- 3rd Submission/int10.sql
+++ 3rd Submission/int10.sql
@@ -1,5 +1,11 @@
---descriçao da interrogaçao
+--
 .mode columns
 .headers on
 .nullvalue NULL
 
+SELECT departamento, COUNT(DISTINCT Docente.id)
+FROM Docente JOIN CadeiraDocente
+WHERE Docente.categoria = 'Professor Auxiliar'
+GROUP BY departamento
+ORDER BY COUNT(DISTINCT Docente.id) DESC
+;
