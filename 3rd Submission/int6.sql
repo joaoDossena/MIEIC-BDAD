7e8c1a021d937df47d00c9bdc77f71ef63e12988
joaoDossena <joaopdossena@gmail.com>
2020-05-23 16:01:46 +0100

    Adding sixth query

--- 3rd Submission/int6.sql
+++ 3rd Submission/int6.sql
@@ -1,5 +1,9 @@
---descriçao da interrogaçao
+--Mostra a média de número de estudantes nas Turmas Práticas com aula na Terça-Feira
 .mode columns
 .headers on
 .nullvalue NULL
 
+SELECT AVG(numEstudantes)
+FROM TurmaPratica
+WHERE diaSemana = 'Terça-Feira'
+;
