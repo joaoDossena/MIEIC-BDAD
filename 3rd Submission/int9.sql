3f2125d210cc1ebc4c83d26638eebae115a7fc46
joaoDossena <joaopdossena@gmail.com>
2020-05-23 17:05:14 +0100

    Adding ninth query

--- 3rd Submission/int9.sql
+++ 3rd Submission/int9.sql
@@ -1,5 +1,10 @@
---descriçao da interrogaçao
+--Lista as Estudantes que se inscreveram na FEUP em 2018 e que podem participar da Tuna Feminina da FEUP (Todas as mulheres)
 .mode columns
 .headers on
 .nullvalue NULL
 
+SELECT id, nome
+FROM PessoaFEUP NATURAL JOIN Estudante
+WHERE PessoaFEUP.sexo = 'F' AND Estudante.anoInscricao = 2018
+
+;
